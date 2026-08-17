43bfde12ec25db0987e87f3512cbfd6e207b53cd
AmitTiwari43 <amit.tiwari23@lpu.in>
2026-08-17 12:39:01 +0530

    fix(test): add missing BeforeEach import in DeliveryLifecycleIntegrationTest

--- src/test/java/com/company/webhooks/DeliveryLifecycleIntegrationTest.java
+++ src/test/java/com/company/webhooks/DeliveryLifecycleIntegrationTest.java
@@ -14,6 +14,7 @@ import com.company.webhooks.event.EventRepository;
 import com.company.webhooks.event.dto.EventResponse;
 import com.company.webhooks.event.dto.IngestEventRequest;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
